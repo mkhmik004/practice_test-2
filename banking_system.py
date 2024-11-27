82a7943f53e393a43203497d241fab398c72a349
Mkhabele Mikejunior Mark Mashaole <mimkhabjhb024@student.wethinkcode.co.za>
2024-11-27 11:10:53 +0200

    all test pass

--- banking_system.py
+++ banking_system.py
@@ -1,23 +1,37 @@
 
 # Function to deposit money into an account
+accounts = {
+            'John': {'balance': 1000},
+            'Jane': {'balance': 500}
+        }
 def deposit(account: dict, amount: float) -> None:
-    pass
+    account['balance']+=amount
 
 # Function to withdraw money from an account
 def withdraw(account: dict, amount: float) -> None:
-    pass
+    if account and account['balance']>=amount:
+        account['balance']-=amount
+    return None
 
 # Function to transfer money between two accounts
 def transfer(from_account: dict, to_account: dict, amount: float) -> None:
-    pass
-
+    if from_account and to_account and from_account['balance']>=amount :
+        from_account['balance']-=amount
+        to_account['balance']+=amount
+    return None
 # Function to add a new account to the system
 def add_account(accounts: dict, owner: str, initial_balance: float) -> None:
-    pass
+    if owner not in accounts:
+        balance={'balance':initial_balance}
+        accounts[owner]=balance
+        pass
 
 # Function to find an account by owner's name
 def find_account(accounts: dict, owner: str) -> dict:
-    pass
+    if owner in accounts:
+        return accounts[owner]
+    else:
+        return None
 
 # Function to display all accounts in the system
 def display_all_accounts(accounts: dict) -> str:
